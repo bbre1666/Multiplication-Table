77831e32d2fa6fe9e72ad6294f4c037322df24f6
bbre1666 <badenbrenner@isu.edu>
2023-01-31 08:24:48 -0700

    Add project files.

---
+++ Multiplication Table/Program.vb
@@ -0,0 +1,24 @@
+Option Explicit On
+Option Strict On
+Imports System
+
+'Baden Brenner
+'RCET0265
+'spring 2023
+'Multiplication Table
+
+
+Module Program
+    Dim size As Integer
+    Sub Main(args As String())
+        Console.WriteLine("what size table do you want?")
+        size = CInt(Console.ReadLine())
+        For col = 1 To size
+            For row = 1 To size
+                Console.Write(Str(col * row).PadLeft(8))
+
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+End Module
